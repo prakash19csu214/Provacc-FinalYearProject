0ed0d85f6124088b98694937bbcb9f89538eaa9b
prakash19csu214 <prakashsingh112000@gmail.com>
2023-03-12 18:31:57 +0530

    kc f

--- src/Components/MasterSearch/index.js
+++ src/Components/MasterSearch/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './index.css'
+import './index.css';
 
 const MasterSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -22,24 +22,29 @@ const MasterSearch = () => {
 
   return (
     <div>
-      <h1>Product Search</h1>
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search products..."
-          value={searchTerm}
-          onChange={handleSearchChange}
-        />
-        <button onClick={handleSearchClick}>Search</button>
+      <div className="d-flex justify-content-center">
+        <h1 className="text-center my-5">Product Search</h1>
+      </div>
+      <div className="d-flex justify-content-center mb-5">
+        <div className="search-container my-3">
+          <input
+            type="text"
+            placeholder="Search products..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+        <button className="btn btn-outline-success my-3 " style={{height: '45px'}} type="submit" onClick={handleSearchClick}>Search</button>
       </div>
       <div className="product-container">
         {products.map(product => (
-          <div key={product.id} className="product-card">
+          <div key={product.id} className="product-card mb-5">
             <a href={product.image} target="_blank" rel="noopener noreferrer">
               <img src={product.image} alt={product.name} />
             </a>
-            <h2>{product.name}</h2>
-            <p>${product.price}</p>
+            <h2 className='my-5 mx-2'>{product.name}</h2>
+            <p className='my-5 mx-2'>Rs.{product.price}</p>
+            <p className='my-5 mx-2'>{product.source}</p>
           </div>
         ))}
       </div>
