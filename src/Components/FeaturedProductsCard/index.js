20cab6b3371b051ad94fb2a9d38a02d253244fcf
prakash19csu214 <prakashsingh112000@gmail.com>
2023-05-19 14:49:28 +0530

    featured products ui done

--- src/Components/FeaturedProductsCard/index.js
+++ src/Components/FeaturedProductsCard/index.js
@@ -1,18 +1,22 @@
 import React from "react";
-import './index.css'
+import "./index.css";
 
-const FeaturedProducts = ({product}) => {
-  // var image = require('../../Assets/Images/'+product.img);
+const FeaturedProducts = ({ product }) => {
+  const handleClick = () => {
+    window.open(product.link, "_blank"); // Open product link in a new tab
+  };
+
   return (
-    <div className='col-4 col-sm-2 mx-3'>
+    <div className="col-4 col-sm-2 mx-3">
       <div className="row d-flex justify-content-center featured">
-        <img src={product.image} className="img-fluid featured-img" />
+        <img src={product.image} className="img-fluid featured-img" alt={product.name} />
+        <div className="overlay" onClick={handleClick}></div> {/* Overlay for click event */}
       </div>
       <div className="text-center featured-head">{product.name}</div>
       <div className="text-center featured-para">
-      {product.source}
-      <br />
-      {product.price}
+        Rs. {product.price}
+        <br />
+        {product.source}
       </div>
     </div>
   );
